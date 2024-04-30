3dff097b7e513dbebc73fb2bbabd8943c0da31a4
ThomasUCN1996 <10503094@ucn.dk>
2024-04-30 09:48:23 +0200

    Employee ALT

--- src/controller/EmployeeController.java
+++ src/controller/EmployeeController.java
@@ -1,5 +1,19 @@
 package controller;
 
-public class EmployeeController {
+import db.DataAccessException;
+import db.EmployeeDAO;
+import db.EmployeeDB;
+import model.Employee;
 
+public class EmployeeController {
+	
+	private EmployeeDAO eDao;
+	
+	public EmployeeController() throws DataAccessException{
+		eDao = new EmployeeDB();
+	}
+	
+	public Employee findByEmployeeNo(int employeeNo) throws DataAccessException{
+		return eDao.findByEmployeeNo(employeeNo);
+	}
 }
