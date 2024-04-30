456e259134a27cc08da53d90e8be90956b317d82
mortenm1337 <150657530+mortenm1337@users.noreply.github.com>
2024-04-30 10:03:05 +0200

    produbtDAO DB

---
+++ src/db/ProductDAO.java
@@ -0,0 +1,13 @@
+package db;
+
+import java.util.List;
+import java.sql.SQLException;
+import model.saleProduct;
+import model.Food;
+import model.Wine;
+
+
+public interface ProductDAO {
+	List<saleProduct> findAll() throws DataAccessException;
+	saleProduct findById(int barcode) throws DataAccessException; 
+}
