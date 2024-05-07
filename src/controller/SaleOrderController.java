e52db6cef0103986bca1970f506e69998f599ef2
ThomasLethO <10503088@ucn.dk>
2024-05-07 09:40:10 +0200

    SaleOrderDB update

--- src/controller/SaleOrderController.java
+++ src/controller/SaleOrderController.java
@@ -2,40 +2,63 @@ package controller;
 
 import db.DataAccessException;
 import db.SaleOrderDB;
+import model.Employee;
 import model.SaleOrder;
+import model.Table;
 import model.saleProduct;
+import db.TableDB;
 
 public class SaleOrderController {
 	private EmployeeController ectrl;
 	private ProductController pctrl;
 	private SaleOrder saleOrder;
 	private SaleOrderDB sodb;
+	private TableDB tadb;
 
-	public SaleOrderController(){
+	public SaleOrderController(){ 
 		try {
 			sodb = new SaleOrderDB();
 			ectrl = new EmployeeController();
 			pctrl = new ProductController();
 			saleOrder = this.saleOrder;
+			
 		} catch (DataAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public SaleOrder createSaleOrder(int employeeNo) throws DataAccessException {
+	public SaleOrder createSaleOrder(int employeeNo, int tableNo) throws DataAccessException {
+		checkTable(tableNo);
+		Employee employee = ectrl.findByEmployeeNo(employeeNo);
+		saleOrder = new SaleOrder(0, 0d, employee, tableNo);
 		return saleOrder;
 	}
 	
-	//Ikke bare void?
-	public saleProduct addProduct(String barcode) throws DataAccessException {
-		return null;
+	public void addProduct(String barcode) throws DataAccessException {
+		
+		
 	}
 	
 	public void saveOrder() throws DataAccessException {
+		
 	}
 	
 	private void updateInventory(int id) {
 		
 	}
+	
+	private void checkTable(int tableNo) throws DataAccessException {
+		Table table = tadb.findByTableNo(tableNo);
+	    if (table.isTableStatus() != true) {
+	        table.setTableStatus(true);
+	        tadb.saveTableStatus(table);
+	        
+	    } else {
+	        throw new DataAccessException("Table:" + tableNo + " not available: ", null);
+	    }
+		
+		
+	}
+	
 }
